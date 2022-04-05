a9340cc8e98f13a28233f9a0d5eb9a755a281c24
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-04-05 15:50:03 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+// FEATURED GAMESHOP
+import { portfolio } from "./settings/data.js";
+
+const portfolioContainer = document.querySelector(".portfolio-container");
+
+portfolio.forEach(work => {
+  portfolioContainer.innerHTML += `
+    <figure class="sample">
+      <div class="sample-photo">
+        <a>
+          <img src="${work.image}" alt="${work.image}" class="sample-img">
+        </a>
+      </div>
+      <div class="sample-content">
+        <h3 class="sample-heading">${work.name}</h3>
+        <div class="sample-title">
+          <div class="year-tag">${work.year}</div>
+          <div class="category">${work.category}</div>
+        </div>
+        <p class="sample-description">${work.description}</p>
+        <div class="btn-wrap">
+        <a class="work-link" id="main" href="${work.prototype}" target="_blank">Prototype</a>
+        <a class="work-link" href="${work.github}" target="_blank">GitHub</a>
+        </div>
+      </div> 
+    </figure>  
+  <hr>     
+  `;
+});
+
+// MOBILE MENU 
 const hamMenu = document.querySelector('.ham-menu');
 const dropMenu = document.querySelector('.drop-menu');
 const dropMenuList = document.querySelectorAll('.drop-menu div');
@@ -23,5 +54,3 @@ dropMenuList.forEach(li => {
   })
 
 })
-
-// API-call here
