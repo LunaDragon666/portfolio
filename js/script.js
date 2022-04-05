11416929d273d8be1d8c637cbe20d641a7dce9b9
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-04-05 19:36:48 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
-const hamMenu = document.querySelector('.ham-menu');
-const dropMenu = document.querySelector('.drop-menu');
-const dropMenuList = document.querySelectorAll('.drop-menu div');
+const hamburgerMenu = document.querySelector('.ham-menu');
+const dropdown = document.querySelector('.drop-menu');
+const dropdownMenu = document.querySelectorAll('.drop-menu div');
 const dropMenuListText = document.querySelectorAll('.drop-menu div a');
 
-hamMenu.addEventListener('click',() => {
-  dropMenu.classList.toggle('drop-down');
-  hamMenu.children[0].classList.toggle('top');
-  hamMenu.children[1].classList.toggle('mid');
-  hamMenu.children[2].classList.toggle('bottom');
+hamburgerMenu.addEventListener('click',() => {
+  dropdown.classList.toggle('drop-down');
+  hamburgerMenu.children[0].classList.toggle('top');
+  hamburgerMenu.children[1].classList.toggle('mid');
+  hamburgerMenu.children[2].classList.toggle('bottom');
 })
 
-dropMenuList.forEach(li => {
-
+dropdownMenu.forEach(li => {
     li.addEventListener('mouseover', (e) => {
     li.style.backgroundImage = "linear-gradient(to right,#EBF5F8,#58A29F)";
     li.children[0].style.color = "white";
   })
-
     li.addEventListener('mouseleave', (e) => {
     li.style.backgroundImage = "linear-gradient(to right,#EBF5F8,#EBF5F8)";
     li.children[0].style.color = "#58A29F";
