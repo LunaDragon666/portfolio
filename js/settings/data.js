b0d83946bf5df45b647dc6c215e2bd09318e0517
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-04-05 22:00:22 +0200

    Update data.js

--- js/settings/data.js
+++ js/settings/data.js
@@ -15,7 +15,7 @@ export const portfolio = [
         category: "Application",
         image: "../img/portfolio/project2.jpg",
         id: "2",
-        description: "In the first semester project, we were to create a website for a fictional science museum aimed at primary school students, families with children, schools and possibly researchers. I've created a visual identity for the Community Science Museum in addition to prototypes of the website both with Adobe XD and as a hosted version of it via Netlify.",
+        description: "Small side-project I worked on when we were learning JavaScript first year in Frontend-program at school. Here I've used public API URL to fetch existing data to create application-like site where users can discover Margarita drinks and how to make them from home, with ingredients that can be bought from nearest liquor- and grocery store.",
         github: "https://github.com/LunaDragon666/margaritas",
         prototype: "https://cdbmargaritas.netlify.app/",
     },
