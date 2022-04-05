3304cecd6ca61041405f828b122dd04d57ce0b70
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-04-05 15:50:05 +0200

    Update data.js

---
+++ js/settings/data.js
@@ -0,0 +1,32 @@
+export const portfolio = [
+    {
+        name: "CS Museum",
+        year: "2021",
+        category: "Webdesign",
+        image: "../img/portfolio/project.jpg",
+        id: "1",
+        description: "In the first semester project, we were to create a website for a fictional science museum aimed at primary school students, families with children, schools and possibly researchers. I've created a visual identity for the Community Science Museum in addition to prototypes of the website both with Adobe XD and as a hosted version of it via Netlify.",
+        github: "https://github.com/LunaDragon666/csm",
+        prototype: "https://csmtrheim.com/",
+    },
+    {
+        name: "Margaritas",
+        year: "2021",
+        category: "Application",
+        image: "../img/portfolio/project2.jpg",
+        id: "2",
+        description: "In the first semester project, we were to create a website for a fictional science museum aimed at primary school students, families with children, schools and possibly researchers. I've created a visual identity for the Community Science Museum in addition to prototypes of the website both with Adobe XD and as a hosted version of it via Netlify.",
+        github: "https://github.com/LunaDragon666/margaritas",
+        prototype: "https://cdbmargaritas.netlify.app/",
+    },
+    {
+        name: "GameHub",
+        year: "2020",
+        category: "UX | UI",
+        image: "../img/portfolio/project3.jpg",
+        id: "3",
+        description: "In the first semester project, we were to create a website for a fictional science museum aimed at primary school students, families with children, schools and possibly researchers. I've created a visual identity for the Community Science Museum in addition to prototypes of the website both with Adobe XD and as a hosted version of it via Netlify.",
+        github: "https://github.com/LunaDragon666/gamehub",
+        prototype: "https://thegamehub.netlify.app/",
+    }
+ ];
